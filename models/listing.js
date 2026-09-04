15ac1b3b7658e90b844f64914a0da470590b31c2
Shivam Kumar Yadav <shivamkumar12ed@gmail.com>
2026-09-04 11:03:29 +0530

    Day 1 - Making Airbnb like mini web

---
+++ models/listing.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    img: {
+        type: String,
+        default: "https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?q=80&w=1175&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        set: 
+        (v) => 
+            v === "" ? "https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?q=80&w=1175&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("listing", listingSchema);
+module.exports = Listing;
