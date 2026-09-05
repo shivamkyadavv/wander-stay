1b11751525b85ccfc43c7edd84255e68624afeb3
Shivam Kumar Yadav <shivamkumar12ed@gmail.com>
2026-09-05 20:36:19 +0530

    Day 2: Add Bootstrap styling and listing cards

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL =  "mongodb://127.0.0.1:27017/wanderfind";
 async function main() {
@@ -21,10 +22,17 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static('public'));
+
 
 app.get("/", (req,res)=> {
     res.send("Hi! I am root");
 });
+
+
+
 
 
 //Index Route
--- models/listing.js
+++ models/listing.js
@@ -7,7 +7,7 @@ const listingSchema = new Schema({
         required: true,
     },
     description: String,
-    img: {
+    image: {
         type: String,
         default: "https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?q=80&w=1175&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         set: 
